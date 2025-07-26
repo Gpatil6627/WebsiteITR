bda310694de237b08ae1ddc4d7a192728b339cbe
Gargi Patil <gargipatil667@gmail.com>
2025-07-26 22:26:48 +0530

    updatedcode26

---
+++ YogaMp/src/assets/Components/MainNav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './MainNav.css';
+
+function MainNav() {
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <img src="/logo.jpg" alt="Logo" />
+      </div>
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/help">Help</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup" className="signup">SignUp</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNav;
---
+++ YogaMp/src/assets/Pages/Home.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import MainNav from '.'; 
+import './Home.css'; // Global styles
+
+function Home() {
+  return (
+    <div>
+      <MainNav/>
+
+      <video autoPlay muted loop id="bgVideo">
+        <source src="/video1.mp4" type="video/mp4" />
+      </video>
+
+      <section className="hero">
+        <div className="hero-content">
+          <h2 className="small-heading">Start a Happy Life</h2>
+          <h1 className="main-heading">
+            Transform Your Mind,<br />Body &amp; Soul—One Breath at a Time
+          </h1>
+          <a href="/explore" className="get-started-btn">Get Started</a>
+        </div>
+      </section>
+
+      <section className="bottom">
+        <div className="bottom-img">
+          <img src="/hm.png" className="hm" alt="yogaWomen" />
+        </div>
+        <div className="right">
+          <h2>
+            Shape Your<br /><span>Perfect Body with YogaTattva</span>
+          </h2>
+          <p className="quote">
+            Yoga is a light, which once lit will never dim.<br />
+            The better your <em>practice</em>, the brighter your <em>flame</em> with YogaTattva
+          </p>
+          <div className="stats">
+            <div className="stat-box">
+              <img src="/client.png" alt="Client Icon" className="icon-img" />
+              <h3>99+</h3>
+              <p>Client Satisfaction</p>
+            </div>
+            <div className="stat-box">
+              <img src="/asan.png" alt="Asan Icon" className="icon-img" />
+              <h3>25+</h3>
+              <p>Unique Asanas</p>
+            </div>
+            <div className="stat-box">
+              <img src="/video.png" alt="Video Icon" className="icon-img" />
+              <h3>35+</h3>
+              <p>Video Tutorials</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
